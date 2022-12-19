c966bd11a2d8342611a351e628c4c661dff747f6
Jiwon Shim <jshim0209@hotmail.com>
2022-12-19 14:29:40 -0500

    initial commit

---
+++ src/test/java/io/jay/eCommerceProject/ECommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jay.eCommerceProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
